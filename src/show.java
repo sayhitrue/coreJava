3aa3f5f3bb3e7a8610c057c4d01de1b1a35d75a5
sayhitrue <sayhitrue@163.com>
2019-03-12 23:26:36 +0800

    Initial commit

---
+++ src/show.java
@@ -0,0 +1,18 @@
+import java.io.*;
+public class show {
+    public static void main(String[] args) throws IOException{
+        String str = "流输出";
+        StringReader inStr = new StringReader(str);
+        StringWriter outStr = new StringWriter();
+        /**
+         * 注意Reader是字符操作，这边要用char，inputstream是字节操作，使用byte
+         */
+        char[] temp = new char[1];
+        while (inStr.read(temp)!=-1){
+            outStr.write(temp);
+        }
+        inStr.close();
+        outStr.close();
+        System.out.print(outStr.toString());
+    }
+}
